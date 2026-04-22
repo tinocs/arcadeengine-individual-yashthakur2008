7d0224ab0ba047217612237ef310d2cb91a4a073
Yash Thakur <130123020+yashthakur2008@users.noreply.github.com>
2026-04-21 21:41:01 -0700

    Update BallWorld.java

--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -15,6 +15,16 @@ public class BallWorld extends World {
         ball.setY(getHeight() / 2 - ball.getHeight() / 2);
         add(ball);
 
+        Ball ball2 = new Ball();
+        ball2.setX(getWidth() / 4 - ball2.getWidth() / 2);
+        ball2.setY(getHeight() / 3 - ball2.getHeight() / 2);
+        add(ball2);
+
+        Ball ball3 = new Ball();
+        ball3.setX(3 * getWidth() / 4 - ball3.getWidth() / 2);
+        ball3.setY(getHeight() / 3 - ball3.getHeight() / 2);
+        add(ball3);
+
         Paddle paddle = new Paddle();
         paddle.setX(getWidth() / 2 - paddle.getWidth() / 2);
         paddle.setY(getHeight() - paddle.getHeight() - 30);
